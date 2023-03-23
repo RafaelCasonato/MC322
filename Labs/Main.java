d9f0382814c598b3eb06ebbe1e00fa6a89892e7f
rafael <rafa.casonato@gmail.com>
2023-03-23 10:24:15 -0300

    Lab 02

---
+++ Labs/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Seguradora teste = new Seguradora("Rafael de Almeida Casonato",  "19949301045", "rafa.casonato@gmail.com", "Avenida Érico Veríssimo");
+        Cliente cliente = new Cliente("Rafael de Almeida Casonato", "467.758.728-01", "31/12/1997", 25, "Avenida Érico Veríssimo");
+        Veiculo veiculo = new Veiculo("HADA1291", "Buggati", "Chiron super sport");
+        Sinistro sinistro = new Sinistro("22/03/2023", "Rua Roxo Moreira 1272");
+        Boolean validacao = cliente.validarCpf("467.758.728-01");
+        System.out.print(cliente);
+        System.out.print("Cpf válido? " + validacao);
+        System.out.print("\n");
+        System.out.print("Id: " + sinistro.getId());
+    }
+}
